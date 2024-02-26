2d458521af31837bf64dce208327a0a0128468f7
Ludvig Hultgren Svensson <ludvig.hs@gmail.com>
2024-02-26 19:11:48 +0100

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,11 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import sys
+
+print(sys.version)
+print(sys.executable)
+
+def greet(who_to_greet):
+    greeting = 'Hello, {}'.format(who_to_greet)
+    return greeting
+
+print(greet('World'))
+print(greet('Ludvig'))
