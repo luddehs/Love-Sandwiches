8f8f125ad77ae0eccac80ea6f36510e3647bcd10
Ludvig Hultgren Svensson <ludvig.hs@gmail.com>
2024-02-28 15:56:36 +0100

    Collect sales data for last 5 markets in lists of columns

--- run.py
+++ run.py
@@ -56,6 +56,16 @@ def validate_data(values):
     return True
 
 
+def update_worksheet(data, worksheet):
+    """
+    Receives a list of integers to be inserted into a worksheet
+    Update the relevant worksheet with the data provided
+    """
+    print(f"Updating {worksheet} worksheet...\n")
+    worksheet_to_update = SHEET.worksheet(worksheet)
+    worksheet_to_update.append_row(data)
+    print(f"{worksheet} worksheet updated successfully\n")
+
 
 def calculate_surplus_data(sales_row):
     """
@@ -77,12 +87,21 @@ def calculate_surplus_data(sales_row):
     return surplus_data
 
 
-def update_worksheet(data, worksheet):
+def get_last_5_entries_sales():
+    """
+    Collects columns of data from sales worksheet, collecting
+    the last 5 entries for each sandwich and returns the data
+    as a list of lists.
+    """
+    sales = SHEET.worksheet("sales")
 
-    print(f"Updating {worksheet} worksheet...\n")
-    worksheet_to_update = SHEET.worksheet(worksheet)
-    worksheet_to_update.append_row(data)
-    print(f"{worksheet} worksheet updated succesfully")
+    columns = []
+    for ind in range(1, 7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+
+    return columns
+
 
 def main():
     """
@@ -92,8 +111,7 @@ def main():
     sales_data = [int(num) for num in data]
     update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)
-    surplus_data = [int(num) for num in new_surplus_data]
-    update_worksheet(surplus_data, "surplus")
+    update_worksheet(new_surplus_data, "surplus")
 
 
 print("Welcome to Love Sandwiches Data Automation")
